cd968da3c48ffda79931524284a1eaec96dc2b06
architolk <msj@xs4all.nl>
2018-09-29 11:12:01 +0200

    checks for commands

--- install.bat
+++ install.bat
@@ -1,12 +1,25 @@
 @echo off
-echo Download fuseki
-curl http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.8.0.zip -o fuseki.zip
-echo Unzip fuseki
-unzip fuseki.zip
-"C:\Program Files\7-Zip\7z.exe" x fuseki.zip
-del fuseki.zip
-ren apache-jena-fuseki-3.8.0 fuseki
-echo Download Dotwebstack
-cd dotwebstack
-curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.33/dotwebstack-theatre-legacy-0.0.33.jar -o dotwebstack.jar
+# First check if all needed commands are available
+where curl >nul 2>nul
+IF ERRORLEVEL 0 (
+  where "C:\Program Files\7-Zip\7z.exe" >nul 2>nul
+  IF ERRORLEVEL 0 (
+    echo Download fuseki
+    curl http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.8.0.zip -o fuseki.zip
+    echo Unzip fuseki
+    unzip fuseki.zip
+    "C:\Program Files\7-Zip\7z.exe" x fuseki.zip
+    del fuseki.zip
+    ren apache-jena-fuseki-3.8.0 fuseki
+    echo Download Dotwebstack
+    cd dotwebstack
+    curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.33/dotwebstack-theatre-legacy-0.0.33.jar -o dotwebstack.jar
+  )
+  ELSE (
+    echo 7z.exe not found in C:\Program Files\7-Zip\
+  )
+)
+ELSE (
+  echo curl not found
+)
 pause
--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-echo Download fuseki
-curl http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.8.0.zip -o fuseki.zip
-echo Unzip fuseki
-unzip fuseki.zip
-rm fuseki.zip
-mv apache-jena-fuseki-3.8.0 fuseki
-echo Download Dotwebstack
-cd dotwebstack
-curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.33/dotwebstack-theatre-legacy-0.0.33.jar -o dotwebstack.jar
+# First check if all needed commands are available
+if hash curl 2>/dev/null; then
+  if hash unzip 2>/dev/null; then
+    echo Download fuseki
+    curl http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.8.0.zip -o fuseki.zip
+    echo Unzip fuseki
+    unzip fuseki.zip
+    rm fuseki.zip
+    mv apache-jena-fuseki-3.8.0 fuseki
+    echo Download Dotwebstack
+    cd dotwebstack
+    curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.33/dotwebstack-theatre-legacy-0.0.33.jar -o dotwebstack.jar
+  else
+    echo "Error: unzip not found"
+  fi
+else
+  echo "Error: curl not found"
+fi
+  
