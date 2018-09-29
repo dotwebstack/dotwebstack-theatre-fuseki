293a10ba30413bc3feb73a85180b5ae09c958316
architolk <msj@xs4all.nl>
2018-09-29 17:00:46 +0200

    Fixed errorlevel bug

--- install.bat
+++ install.bat
@@ -1,25 +1,22 @@
 @echo off
-# First check if all needed commands are available
-where curl >nul 2>nul
-IF ERRORLEVEL 0 (
-  where "C:\Program Files\7-Zip\7z.exe" >nul 2>nul
-  IF ERRORLEVEL 0 (
+rem First check if all needed commands are available
+where /Q curl
+IF %ERRORLEVEL%==0 (
+  dir "C:\Program Files\7-Zip\7z.exe" >nul 2>nul
+  IF %ERRORLEVEL%==0 (
     echo Download fuseki
     curl http://www-eu.apache.org/dist/jena/binaries/apache-jena-fuseki-3.8.0.zip -o fuseki.zip
     echo Unzip fuseki
-    unzip fuseki.zip
     "C:\Program Files\7-Zip\7z.exe" x fuseki.zip
     del fuseki.zip
     ren apache-jena-fuseki-3.8.0 fuseki
     echo Download Dotwebstack
     cd dotwebstack
     curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.33/dotwebstack-theatre-legacy-0.0.33.jar -o dotwebstack.jar
-  )
-  ELSE (
+  ) ELSE (
     echo 7z.exe not found in C:\Program Files\7-Zip\
   )
-)
-ELSE (
+) ELSE (
   echo curl not found
 )
 pause
