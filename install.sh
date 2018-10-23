637476837a060d7643253f1f201f42bdeded56ba
architolk <msj@xs4all.nl>
2018-10-23 19:30:24 +0200

    Increased max upload size

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ if hash curl 2>/dev/null; then
     unzip fuseki.zip
     rm fuseki.zip
     mv apache-jena-fuseki-3.8.0 fuseki
+    echo copy resources
+    cp resources/jetty-web.xml fuseki/webapp/WEB-INF
     echo Download Dotwebstack
     cd dotwebstack
     curl -L https://github.com/dotwebstack/dotwebstack-theatre-legacy/releases/download/v0.0.34/dotwebstack-theatre-legacy-0.0.34.jar -o dotwebstack.jar
